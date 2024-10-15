9bd78a1f0f44994b255c51487f0a4be7740e653f
Sean Dooley <s.dooley@ctidigital.com>
2024-10-15 15:01:42 +0100

    Bindings and forms

---
+++ idine/CheckoutView.swift
@@ -0,0 +1,34 @@
+//
+//  CheckoutView.swift
+//  idine
+//
+//  Created by Sean Dooley on 15/10/2024.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    @State private var paymentType = "Cash"
+    
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    CheckoutView()
+        .environmentObject(Order())
+}
