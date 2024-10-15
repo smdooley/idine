d9df4253cfda60e13e2b8a06f6b17dc0ddfe3243
Sean Dooley <s.dooley@ctidigital.com>
2024-10-15 11:04:04 +0100

    Initial Commit

---
+++ idine/idineApp.swift
@@ -0,0 +1,17 @@
+//
+//  idineApp.swift
+//  idine
+//
+//  Created by Sean Dooley on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct idineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
