de9855081eb09666f58ad3dece78ab7da62af0d7
Sean Dooley <s.dooley@ctidigital.com>
2024-10-15 11:49:45 +0100

    Displaying a detail screen with NavigationLink

---
+++ idine/ItemDetail.swift
@@ -0,0 +1,39 @@
+//
+//  ItemDetail.swift
+//  idine
+//
+//  Created by Sean Dooley on 15/10/2024.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    let item: MenuItem
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit()
+                Text("Photo: \(item.photoCredit)")
+                    .padding(4)
+                    .background(.black)
+                    .font(.caption)
+                    .foregroundStyle(.white)
+                    .offset(x: -5, y: -5)
+            }
+            Text(item.description)
+                .padding()
+            Spacer()
+        }
+        .navigationTitle(item.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ItemDetail(item: MenuItem.example)
+    }
+}
